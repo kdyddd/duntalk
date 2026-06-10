a2dcf34f5b5728582039753d8ed6e9251af5e0f0
kdyddd <kdyddd@gmail.com>
2026-06-10 14:41:44 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/duntalk/DuntalkApplication.java
@@ -0,0 +1,13 @@
+package com.duntalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuntalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuntalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duntalk/DuntalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duntalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuntalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
